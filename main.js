06ea2a80cc6661be96fb61747c356d86f0be2536
Derrick Holt <holtshady@gmail.com>
2023-02-25 12:39:16 -0500

    finished all, alert set on dom load to instruct user

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-/* Imports */
 
 import {pAequorFactory, returnRandBase, mockUpStrand} from './modules/DNA-Logic.mjs'
 
-
 const button = document.querySelector('.buttonChildContainer button');
-
 const pairDisplayContainer = document.getElementsByClassName('pairDisplayContainer')[0];
 
-/* Generate strands button and Strand rendering */
+document.addEventListener('DOMContentLoaded', function() {
+  window.alert('Generate new strands first before attempting to use the pair selector');
+});
 const strand1TXT = document.getElementById('strand1');
 const strand2TXT = document.getElementById('strand2');
+let organism, newOrganismComplement, isOrganismGenerated = false;
 
 const newOrganism = () => {
   const num = Math.floor(Math.random() * 2);
   const dna = mockUpStrand();
-  return pAequorFactory(num, dna);
+  organism = pAequorFactory(num, dna);
+  newOrganismComplement = organism.complementStrand();
 };
-const organism = newOrganism();
-const newOrganismComplement = organism.complementStrand();
 
 button.addEventListener('mousedown', () => {
   button.style.backgroundColor = '#8F7281';
   button.style.boxShadow = '1px 2px rgb(175, 175, 175)';
   button.style.transform = 'translate(2px, 3px)';
-  const organism = newOrganism();
-  strand1TXT.innerHTML = "Base Strand: " + organism.DNA.join(" ");
-  strand1TXT.style.marginRight = '50px';
-  const newOrganismComplement = organism.complementStrand();
-  strand2TXT.innerHTML = "Complementary Strand: " + newOrganismComplement.DNA.join(" ");
+  newOrganism();
+  strand1TXT.innerHTML = organism.DNA.join(" ");
+  strand2TXT.innerHTML = newOrganismComplement.DNA.join(" ");
+  isOrganismGenerated = true;
 });
 
 button.addEventListener('mouseup', () => {
@@ -36,24 +34,33 @@ button.addEventListener('mouseup', () => {
     button.style.transform = 'none';
 });
 
-
-
 console.log(organism)
 console.log(newOrganismComplement)
 
-/*   pairDisplayContainer.style.animation = 'close 1s linear forwards, 1s 1.5s open linear forwards';
- this is for the pair selection display animation */
-
 /* Dropdown for pair selection */
 const btn = document.getElementsByClassName('btn')[0];
-const dropdown = document.getElementsByClassName('dropdown-content')[0];
 const form = document.getElementById('form');
-
-btn.addEventListener('click', () => {
-  dropdown.classList.toggle('show');
-});
+const pairBase = document.getElementById('pairStrand1');
+const pairComp = document.getElementById('pairStrand2');
+let isAnimating = false;
 
 form.addEventListener('change', (event) => {
+  if (isAnimating) return;
+  isAnimating = true;
+  setTimeout(() => {
+    isAnimating = false;
+  }, 2500);
+
   btn.textContent = event.target.value;
-  dropdown.classList.remove('show');
+  pairDisplayContainer.style.animation = 'close 1s linear forwards, 1s 1.5s open linear forwards';
+  setTimeout(() => {
+    let selectedValue = event.target.value;
+    let arrValue = selectedValue.split("-");
+    let arrValueNum = parseInt(arrValue[1]) - 1;
+    pairBase.innerHTML = 'Base :' + organism.DNA[arrValueNum];
+    pairComp.innerHTML = 'Comp :' + newOrganismComplement.DNA[arrValueNum];
+  }, 1200);
+  setTimeout(() => {
+    pairDisplayContainer.style.animation = '';
+  }, 2500)
 });
